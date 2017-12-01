1816bcfe1acb4ac31dc70004b54c84c40276fdcc
Patrik Nordlén <patriki@gmail.com>
2017-12-02 00:23:26 +0100

    Improve argument parsing

--- util/addstyles.py
+++ util/addstyles.py
@@ -6,13 +6,25 @@ import sys
 import os
 import shutil
 from tempfile import mkdtemp
+from argparse import ArgumentParser
 
 def main(args):
-    if len(args) < 3:
-        print "Usage: %s <file to patch> <stylefile>" % args[0]
-        return
+    argparser = ArgumentParser(description='A tool for patching a docx file with a set of chosen styles ' \
+                                           'so that they can be referenced within the document.xml part of ' \
+                                           'the docx file.')
+    argparser.add_argument('-i', '--infile', required=True, help='docx file to patch')
+    argparser.add_argument('-s', '--stylefile', required=True, help='File containing the set of styles that should be added ' \
+                                                     '(<w:style> elements)')
+    argparser.add_argument('-o', '--outfile', help='Patched docx file to write as output')
+    argparser.add_argument('-r', '--replace', help='Write patched results back to original docx file', action='store_true')
 
-    docxfile = args[1]
+    args = argparser.parse_args()
+
+    if args.outfile is None and args.replace is None:
+        argparser.print_usage()
+        sys.exit(0)
+
+    docxfile = args.infile
     tmpdir = mkdtemp()
 
     try:
@@ -22,12 +34,12 @@ def main(args):
     except:
         raise Exception("ERROR: this doesn't look like a valid docx file!")
 
-    customstylefile = args[2]
+    customstylefile = args.stylefile
     try:
         with open(customstylefile) as stylefile:
             customstyles = stylefile.read().rstrip()
     except:
-        raise Exception("ERROR: couldn't read style file %s" % args[2])
+        raise Exception("ERROR: couldn't read style file %s" % customstylefile)
 
     with open("%s/word/styles.xml" % tmpdir,"r+") as docxstylefile:
         docxstyles = docxstylefile.read()
@@ -38,7 +50,11 @@ def main(args):
         docxstylefile.seek(0)
         docxstylefile.write(docxstyles)
 
-    outfile = "%s_patched%s" % os.path.splitext(docxfile)
+    if args.replace:
+        outfile = args.infile
+    else:
+        outfile = args.outfile
+
     z = zipfile.ZipFile(outfile, "w", compression=zipfile.ZIP_DEFLATED)
     os.chdir(tmpdir)
     for dirname, subdirs, files in os.walk('.'):
